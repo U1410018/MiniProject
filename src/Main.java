d96690c3cd4dc1f11e2da085c79b4672f707febf
Muhammadjon <tohirov.muhammadjon@mail.ru>
2017-12-09 22:08:38 +0900

    backup

---
+++ src/Main.java
@@ -0,0 +1,19 @@
+/**
+ * 
+ */
+
+/**
+ * @author Muhammadjon
+ *
+ */
+public class Main {
+
+	/**
+	 * @param args
+	 */
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		
+	}
+
+}
